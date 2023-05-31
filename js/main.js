4e1dffb579c56ca391e10dfb589b55e38675a196
MiriamPaternain <miriampaternain@gmail.com>
2023-05-31 09:04:16 +0200

    add basic structure files

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/* 
+1.estructura html: main, h1, h4, select, input, submit, h2x3 con clase hidden, p
+2.clases css: hidden para las frases
+3.estilos de la web
+4.crear constantes: select, input, submit, p(textos), saldo
+5.eventos: click sobre jugar
+6.funciones: numero aleatorio, si acierta: frase has ganado, se le suma el doble de lo apostado; si pierde: frase has perdido, se le resta lo apostado
+*/
+
+'script'
+
+//Variables:
+
+
+//Funciones:
+
+
+//Eventos:
